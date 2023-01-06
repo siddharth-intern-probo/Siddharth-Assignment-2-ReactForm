3b75ff48c5511f8b75e10fa87632304ace9e5dc3
Siddharth Bihani <121893109+siddharth-intern-probo@users.noreply.github.com>
2023-01-06 11:20:09 +0530

    Updated Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,83 +1,48 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function Form() {
-	const [userName , setUserName] = useState('');
-	const [name , setName] = useState('');
-	const [phone , setPhone] = useState('');
-	const [password , setPassword] = useState('');
-	const [confPassword , setConfPassword] = useState('');
-
-	// function to update state of userName with
-	// value enter by user in form
-	const handleUserNameChange =(e)=>{
-	    setUserName(e.target.value);
-	}
-	// function to update state of name with value
-	// enter by user in form
-	const handleNameChange =(e)=>{
-	    setName(e.target.value);
-	}
-	// function to update state of phone with value
-	// enter by user in form
-	const handlePhoneChange =(e)=>{
-	    setPhone(e.target.value);
-	}
-	// function to update state of password with
-	// value enter by user in form
-	const handlePasswordChange =(e)=>{
-	    setPassword(e.target.value);
-	}
-	// function to update state of confirm password
-	// with value enter by user in form
-	const handleConfPasswordChange =(e)=>{
-	    setConfPassword(e.target.value);
-	}
-	// below function will be called when user
-	// click on submit button .
-	const handleSubmit=(e)=>{
-        if(password!=confPassword)
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const userName = e.target.userName.value;
+		const fullName = e.target.fullName.value;
+		const phone = e.target.phone.value;
+		const password = e.target.password.value;
+		const confPassword = e.target.confPassword.value;
+		if(password !== confPassword)
         {
-            // if 'password' and 'confirm password'
-            // does not match.
+            // if 'password' and 'confirm password' does not match.
             alert("password Not Match");
         }
         else{
-            // display alert box with user
-            // 'name' and 'email' details .
-            alert('A form was submitted with UserName: "' + userName + '", Name: "'+name +'" and Phone: "' + phone + '"');
+            // display alert box with username, name and phone details .
+            alert('A form was submitted with UserName: "' + userName + '", Name: "'+ fullName +'" and Phone: "' + phone + '"');
         }
-        e.preventDefault();
-	}
+	};
 
 return(
 	<div className="Form">
-	    <form style={{padding: "250px 0"}} onSubmit={(e) => {handleSubmit(e)}}>
+	    <form style={{padding: "250px 0"}} onSubmit={handleSubmit}>
 	        {/*when user submit the form , handleSubmit() function will be called .*/}
             <label >
-            User Name:
+            	User Name:
             </label><br/>
-            <input type="text" value={userName} required onChange={(e) => {handleUserNameChange(e)}} /><br/>
-                { /*when user writea in username input box, handleUserNameChange() function will be called. */}
+            <input type="text" name="userName" required/><br/>
             <label >
-            Name:
+            	Name:
             </label><br/>
-            <input type="text" value={name} required onChange={(e) => {handleNameChange(e)}} /><br/>
-                { /*when user write in name input box, handleAgeChange() function will be called. */}
+            <input type="text" name="fullName" required/><br/>
             <label>
-            Phone:
+            	Phone:
             </label><br/>
-            <input type="text" value={phone} required onChange={(e) => {handlePhoneChange(e)}} /><br/>
-                {/* when user write in phone input box, handlePhoneChange() function will be called.*/}
+            <input type="text" name="phone" required/><br/>
             <label>
-            Password:
+           		Password:
             </label><br/>
-            <input type="password" value={password} required onChange={(e) => {handlePasswordChange(e)}} /><br/>
-                {/* when user write in password input box, handlePasswordChange() function will be called.*/}
+            <input type="password" name="password" required /><br/>
             <label>
-            Confirm Password:
+            	Confirm Password:
             </label><br/>
-            <input type="password" value={confPassword} required onChange={(e) => {handleConfPasswordChange(e)}} /><br/>
-                {/* when user write in confirm password input box, handleConfPasswordChange() function will be called.*/}
+            <input type="password" name="confPassword" required/><br/>
             <input type="submit" value="Submit"/>
 	    </form>
 	</div>
